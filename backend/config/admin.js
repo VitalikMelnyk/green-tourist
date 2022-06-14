1a2e6d649f286c3f21538437510815fab8fb08b3
VitalikMelnyk <melnyk.vitalikk@gmail.com>
2022-06-14 12:32:14 +0300

    refactoring

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '5c4b44231edf7d61116152de7194d831'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
